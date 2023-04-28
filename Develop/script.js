50dfed20e62790baf80ba0ee446dc048781cc3ee
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-04-28 19:58:59 -0400

    added a passCriteria function to collect input from the user on button click was starting to add special lowers and upper and numerics arrays to hold some of the passsword selection

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,25 @@
 // Assignment code here
+const specials = [];
+const lowers = [];
+const uppers = [];
+const numerics = [];
+
+
+function passCriteria(){
+  passNum = prompt("How long is your password? "+"A minimum of 8 please! " ),passSpecial = prompt("Would you like special characters?")
+  ,passLower = prompt("Would you like lowercase letters?"),passUpper = prompt("Would you like uppercase letters?")
+  ,passNum = prompt("Would you like numbers?")
+}
+
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -15,3 +27,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passCriteria);
+
